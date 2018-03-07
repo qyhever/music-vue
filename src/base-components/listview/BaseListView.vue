3df9363ea886b8228473a0d4ce704c8641dd0920
query <15927700473@163.com>
2018-03-07 16:53:58 +0800

    完成BaseListView.vue左右列表联动

--- src/base-components/listview/BaseListView.vue
+++ src/base-components/listview/BaseListView.vue
@@ -1,6 +1,10 @@
 <template>
   <!-- 1.1 使用scroll组件 -->
-  <scroll class="listview">
+  <scroll class="listview"
+          ref="listview"
+          :probe-type="probeType"
+          :listen-scroll="listenScroll"
+          @scroll="scroll">
     <!-- 1.0 左边歌手列表结构 -->
     <ul>
 			<li class="list-group" v-for="(group, index) in data" :key="index" ref="listGroup">
@@ -13,12 +17,29 @@
 				</ul>
 			</li>
 		</ul>
-
+    <!-- 2.0 右侧字母列表结构 -->
+    <div class="list-shortcut"
+          @touchstart="onShortcutTouchStart"
+          @touchmove.stop.prevent="onShortcutTouchMove">
+			<ul>
+				<li class="item"
+						v-for="(item, index) in shortcutList"
+						:key="index"
+						:data-index="index"
+						:class="{'current': currentIndex === index}">
+            <!-- 4.4 添加样式 -->
+					{{item}}
+				</li>
+			</ul>
+		</div>
   </scroll>
 </template>
 
 <script>
 import Scroll from 'base-components/scroll/BaseScroll';
+import {getData} from 'common/js/dom';
+
+const ANCHOR_HEIGHT = 18;
 export default {
   components: {Scroll},
 	props: {
@@ -27,8 +48,92 @@ export default {
       default: []
     }
   },
+  data() {
+    return {
+      scrollY: -1,
+      currentIndex: 0
+    };
+  },
+  computed: {
+    // 2.1 取得字母列表数据
+    shortcutList() {
+      return this.data.map(value => {
+        return value.title.substr(0, 1);
+      });
+    }
+  },
+  watch: {
+    // 4.2 数据变化时，需要从新计算高度
+    data() {
+      setTimeout(() => {
+        this._calcHeight();
+      });
+    },
+    // 4.3 观测当前的高度，得到范围之间的索引
+    scrollY(newY) {
+      const listHeight = this.listHeight;
+      for (let i = 0; i < listHeight.length; i++) {
+        const height1 = listHeight[i];
+        const height2 = listHeight[i + 1];
+        if (!height2 || (-newY > height1 && -newY < height2)) {
+          this.currentIndex = i;
+          return;
+        }
+      }
+      this.currentIndex = 0;
+    }
+  },
   created() {
-    
+    this.touch ={};
+    this.probeType = 3;
+    this.listenScroll = true;
+		this.listHeight = [];
+  },
+  mounted() {
+    // setTimeout(() => {
+    //   this._calcHeight();
+    // }, 20);
+  },
+  methods: {
+    // 3.0 点击字母
+    onShortcutTouchStart(e) {
+      let anchorIndex = getData(e.target, 'index'); // 获取到的自定义index是String
+      console.log(anchorIndex)
+      let firstTouch = e.touches[0];
+      this.touch.y1 = firstTouch.pageY;
+      this.touch.anchorIndex = anchorIndex;
+      this._scrollTo(anchorIndex);
+    },
+    // 3.1 字母列表滑动
+    onShortcutTouchMove(e) {
+      let firstTouch = e.touches[0];
+      this.touch.y2 = firstTouch.pageY;
+      let delta = (this.touch.y2 - this.touch.y1) / ANCHOR_HEIGHT | 0; // 手指滑动了多少个字母
+      let anchorIndex = +this.touch.anchorIndex + delta;
+      this._scrollTo(anchorIndex);
+    },
+    // 4.0 获取scroll子组件传过来的pos位置，此前需要设置probeType = 3，listenScroll = true
+    scroll(pos) {
+      this.scrollY = pos.y;
+    },
+    // 4.1 计算每一项的高度并放到数组里
+    _calcHeight() {
+      this.listHeight = [];
+      const list = this.$refs.listGroup;
+      let height = 0;
+      this.listHeight.push(height);
+      if (list) {
+        for (let i = 0; i < list.length; i++) {
+          const item = list[i];
+          height += item.clientHeight;
+          this.listHeight.push(height);
+        }
+      }
+    },
+    // 传入索引让容器滚动到对应位置
+    _scrollTo(index) {
+      this.$refs.listview.scrollToElement(this.$refs.listGroup[index], 0);
+    }
   }
 }
 </script>
