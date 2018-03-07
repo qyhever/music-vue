197629e1ff3c0f87dc6946a5e70161dc8706a344
query <15927700473@163.com>
2018-03-07 10:00:46 +0800

    项目工程目录搭建，完成MHeader.vue组件

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <m-header></m-header>
+    <keep-alive>
+      <router-view/>
+    </keep-alive>
+  </div>
+</template>
+
+<script>
+import MHeader from 'components/m-header/MHeader';
+export default {
+  components: {MHeader}
+}
+</script>
+
+<style lang="stylus">
+@import "~common/stylus/variable"
+#app
+  color: $color-theme
+</style>
