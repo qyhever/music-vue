d2c161209fe21944bccd783c03f09b03192af416
query <15927700473@163.com>
2018-03-13 21:18:56 +0800

    MusicList.vue组件开发一

--- src/components/singer-detail/SingerDetail.vue
+++ src/components/singer-detail/SingerDetail.vue
@@ -1,8 +1,6 @@
 <template>
   <transition name="slide">
-    <div class="singer-detail">
-      
-    </div>
+    <music-list :title="title" :bg-image="bgImage" :songs="songs"></music-list>
   </transition>
 </template>
 
@@ -11,7 +9,9 @@ import {mapGetters} from 'vuex';
 import {getSingerDetail} from 'api/singer';
 import {ERR_OK} from 'api/config';
 import {createSong} from 'common/js/song';
+import MusicList from 'components/music-list/MusicList';
 export default {
+  components: {MusicList},
   data() {
     return {
       songs: []
@@ -20,7 +20,13 @@ export default {
   computed: {
     ...mapGetters([
       'singer'
-    ])
+    ]),
+    title() {
+      return this.singer.name;
+    },
+    bgImage() {
+      return this.singer.avatar;
+    }
   },
   created() {
     // this.id = this.$route.params.id;
